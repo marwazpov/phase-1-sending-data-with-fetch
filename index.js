38704cb203e7b336451202d527d1cad3a9f9a850
marwa <mds08@mail.aub.edu>
2022-09-02 11:17:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  const data = {
+      name: userName,
+      email: userEmail
+  }
+
+  const configurationObject = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+  };
+  return fetch('http://localhost:3000/users', configurationObject)
+      .then(res => res.json())
+      .then(data => {
+          const paragraph = document.createElement('p')
+          paragraph.innerHTML = `New ID: ${data.id}`
+          document.querySelector('script').insertBefore(paragraph, document.querySelector('script').childNodes[0])
+      })
+      .catch(function(error){
+          document.querySelector('body').innerHTML = `<p> error: ${error.message} </p>`
+      })
+  }
+
+  submitData('Marwa','@gmail')
